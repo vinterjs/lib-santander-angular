5762cfe1c36054773ef125cce4deb6b0c6b0dcb7
Francisco Hormazabal <franciscohormazabal@MacBook-Air-de-Cristina-Gonzalez.local>
2020-09-21 10:45:54 -0300

    add: new branch

---
+++ projects/ui-santander/src/lib/components/prueba/prueba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-prueba',
+  templateUrl: './prueba.component.html',
+  styleUrls: ['./prueba.component.css']
+})
+export class PruebaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/ui-santander/src/lib/ui-santander.module.ts
+++ projects/ui-santander/src/lib/ui-santander.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { UiSantanderComponent } from './ui-santander.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
 
 
 @NgModule({
-  declarations: [UiSantanderComponent],
+  declarations: [UiSantanderComponent, PruebaComponent],
   imports: [
   ],
   exports: [UiSantanderComponent]
